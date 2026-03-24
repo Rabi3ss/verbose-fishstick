dd1cf14277087d6b74685467ae16dae04e04c619
rabi3 <rabielyamani00@gmail.com>
2026-03-24 13:45:09 +0100

    finsh the strrchr.c 100% nadiya

--- strrchr.c
+++ strrchr.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
 
-char *ft_strrch(char *s, int c);
+char *ft_strrchr(char *s, int c);
 
 int main()
 {
+	printf("%s", ft_strrchr("fin cv hni cv bikhir", 98)); 
+}
 
+char *ft_strrchr(char *s, int c)
+{
+	char *last = 0;
+	while(1)
+	{
+		if(*s == c)
+			last = s;
+		if(*s == '\0')
+			return last;
+		s++;
+	}
+	return '\0';
 }
