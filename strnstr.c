a2cf135b38be4fdcbd25869082da86c64b1952bb
rabi3 <rabielyamani00@gmail.com>
2026-03-26 23:36:58 +0100

    finsh the strnstr.c 100%

--- strnstr.c
+++ strnstr.c
@@ -4,30 +4,27 @@ char *ft_strnstr(char *big, char *little, size_t len);
 
 int main()
 {
-
+	printf("%s", ft_strnstr("ha fin bikhir cvv 3lik", "han", 4));
 }
-
-if(*little == '\0')
-	return big;
-
-while(*little > 0 && *little < 33)
-	little++;
 
 char *ft_strnstr(char *big, char *little, size_t len)
 {
+	//if the empty (farigha) return big
+	if(*little == '\0')
+		return big;
 	int i = 0;
-	int all_word = 0;
+	int j = 0;
 
-	while(*big)
+	while(big[i] && i < len)
 	{
-		while(*big = little[i] && little[i] && *big && len >= all_word )
+		j = 0;
+		while(big[i + j] == little[j] && little[j] && i + j < len)
 		{
-			big++;
-			len++;
-			i++;
+			j++;
 		}
-		if(little[i] == '\0')
-			return little; 
+		if(little[j] == '\0')
+			return &big[i];
+		i++;
 	}
 	return NULL;
 }
