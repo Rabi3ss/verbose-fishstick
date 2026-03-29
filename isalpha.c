182ccc35ee690f5ce645b965594638956f37e00b
rabi3 <rabielyamani00@gmail.com>
2026-03-29 14:13:30 +0100

    finsh the isalpha.c 100%

---
+++ isalpha.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int ft_isalpha(int c);
+
+int main()
+{
+
+}
+
+int ft_isalpha(int c)
+{
+	if((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+		return 1;
+	else
+		return 0;
+}
