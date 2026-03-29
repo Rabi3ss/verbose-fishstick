5f3a8ec7b641e189146828dd8f2515b97d44d35f
rabi3 <rabielyamani00@gmail.com>
2026-03-29 17:31:12 +0100

    finsh the bzero.c 100%

---
+++ bzero.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void *ft_bzero(void *s, size_t n);
+
+int main()
+{
+
+}
+
+void ft_bzero(void *s, size_t n)
+{
+	unsigned char *ptr = s;
+
+	while(n  > 0)
+	{
+		*ptr = '\0';
+		ptr++;
+		n--;
+	}
+}
