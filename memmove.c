b4ce336a7a6bd7619cab49113e698b70530d5482
rabi3 <rabielyamani00@gmail.com>
2026-03-30 22:21:01 +0100

    finsh the memmove.c 100%

---
+++ memmove.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+void *ft_memmove(void *dest, const void *src, size_t n);
+
+int main()
+{
+
+}
+
+void *ft_memmove(void *dest, const void *src, size_t n)
+{
+	unsigned char		*ldest;
+	const unsigned char	*lsrc;
+	int			i;
+	
+	ldest = (unsigned char *)dest;
+	lsrc = (unsigned char *)src;
+	if (!ldest && !lsrc)
+		return 0;
+	i = 0;
+	if (ldest > lsrc)
+	{
+		while (n > 0)
+		{
+			n--;
+			ldest[n] = lsrc[n];
+		}
+		return dest;
+	}	
+	while (n > 0)
+	{
+		ldest[i] = lsrc[i];
+		i++;
+		n--;
+	}
+	return dest;
+}
