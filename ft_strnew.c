25ef87053fe704e4df81dc5c1ccfb88786fabf43
rabi3 <rabielyamani00@gmail.com>
2026-03-31 16:09:20 +0100

    finsh the ft_strnew.c 100%

---
+++ ft_strnew.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+char *ft_strnew(size_t size);
+
+int main()
+{
+
+}
+
+char *ft_strnew(size_t size)
+{
+	char 	*str;
+	size_t		i;
+	
+	i = 0;
+	str = (char *)malloc(size + 1);
+	if (str == NULL)
+		return NULL;
+	while (size >= i)
+	{
+		str[i] = '\0';
+		i++;
+	}
+	return str;
+}
+
+
+
