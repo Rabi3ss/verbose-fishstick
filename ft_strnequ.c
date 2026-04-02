f06dedef841a5d0eddf2683df724a34d1d7bff5c
rabi3 <rabielyamani00@gmail.com>
2026-04-02 14:05:01 +0100

    finsh the ft_strnequ.c 100%

---
+++ ft_strnequ.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int	ft_strnequ(char const *s1, char const *s2, size_t n);
+
+int main()
+{
+
+}
+
+int	ft_strnequ(char const *s1, char const *s2, size_t n)
+{
+	if (!s1 || !s2)
+		return 0;
+	if (n == 0)
+		return 1;
+
+	while (n > 1 && *s1 == *s2 && *s1)
+	{
+		s1++;
+		s2++;
+		n--;
+	}
+	if (*s1 == *s2)
+		return 1;
+	return 0;
+}
+
+
