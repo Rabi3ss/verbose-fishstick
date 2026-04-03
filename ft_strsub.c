a084529b798779bcaeb9bc780e93075343f47cb5
rabi3 <rabielyamani00@gmail.com>
2026-04-03 12:24:10 +0100

    finsh the ft_strnsub.c 100%

---
+++ ft_strsub.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+char	*ft_strsub(char const *s, unsigned int start, size_t len);
+int	ft_strlen(char *s);
+
+int	main()
+{
+
+}
+
+int	ft_strlen(char *s)
+{
+	int 	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return i;
+}
+
+char	*ft_strsub(char const *s, unsigned int start, size_t len)
+{
+	char		*str;
+	int		i;
+	unsigned int	IsIn;
+
+	IsIn = ft_strlen(s);
+	i = 0;
+	if (start > IsIn) return NULL;
+	if (IsIn < start + len) return NULL;
+	str = malloc(len * sizeof(char) + 1);
+	if (str == NULL) return NULL;	
+	while (len > 0)
+	{
+		str[i] = s[start + i];
+		len--;
+		i++;
+	}
+	str[i] = '\0';
+	return str;
+
+}
