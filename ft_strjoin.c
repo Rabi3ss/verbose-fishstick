b8c4ef5c75670d2ca3cb5ee17dd98fa9284392e2
rabi3 <rabielyamani00@gmail.com>
2026-04-03 12:48:45 +0100

    finsh the ft_strjoin.c 100%

---
+++ ft_strjoin.c
@@ -0,0 +1,43 @@
+char	*ft_strjoin(char const *s1, char const *s2);
+int	ft_strlen(char *s);
+
+int	ft_strlen(char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return i;
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	if (!s1 || !s2) return NULL;
+	char	*str;
+	int	len1;
+	int	len2;
+	int	i;
+
+
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	i = 0;
+	str = malloc((len1 + len2 + 1) * sizeof(char));
+	if (str == NULL) return NULL;
+	while (len1 > i)
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	while (len2 > 0)
+	{
+		str[i] = *s2;
+		i++;
+		s2++;
+		len2--;
+	}
+	str[i] = '\0';
+	return str;
+
+}
