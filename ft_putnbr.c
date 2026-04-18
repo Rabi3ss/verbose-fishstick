2fbab10f512c73f915a3f731f5bb2c9dfcc756bd
rabi3 <rabielyamani00@gmail.com>
2026-04-19 00:21:38 +0100

    finsh the ft_putnbr.c but i use long to don't do while for max and mini int

---
+++ ft_putnbr.c
@@ -0,0 +1,30 @@
+#include <unistd.h>
+void  ft_putnbr(int n);
+
+int main()
+{
+  ft_putnbr(-2147483648);
+}
+
+
+void  ft_putnbr(int n)
+{
+  long ln;
+  char c;
+  char  N;
+  ln = n;
+  
+  N = '-';
+  if (n < 0)
+  {
+     write(1, &N, 1);
+    ln *= (-1);
+  }
+  if ((ln / 10) != 0)
+  {
+    ft_putnbr(ln / 10);
+  }
+  c = (ln % 10) + '0';
+  write(1, &c, 1);
+}
+
